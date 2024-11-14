67df1b2ed4e1b012df1cb3df982ec64ecfacd6ce
Abinadabe Oliveira <abinadabedev@outlook.com>
2024-11-13 23:33:13 -0300

    novo commit

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,9 @@
 import br.com.alura.screenmatch.models.Filme;
+import br.com.alura.screenmatch.models.Serie;
 
 public class Principal {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
+        /*Filme meuFilme = new Filme();
         meuFilme.setNome("God Of War");
 
         meuFilme.setAnoDeLancamento(2025);
@@ -15,5 +16,16 @@ public class Principal {
         meuFilme.avalia(10);
         System.out.println("Total de Avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.obterMedia());
+         */
+
+        Serie Suits = new Serie();
+        Suits.setNome("Suits");
+        Suits.setAnoDeLancamento(2016);
+        Suits.exibeFichaTecnica();
+        Suits.setTemporadas(10);
+        Suits.setEpisodiosPorTemporada(15);
+        Suits.setMinutosPorEpisodio(45);
+
+        System.out.println("Duração para Maratonar Suits: " + Suits.getDuracaoEmMinutos());
     }
 }
